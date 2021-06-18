7eb4c77a13b509b0bf8252a45e3f57894bc52222
Marinel maxime <maxime.m62@gmail.com>
2021-06-18 16:37:25 +0200

    Try change router

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  //router: {
-  //   base: '/barista/'
-  //},
+  router: {
+     base: '/barista/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'barista-site',
