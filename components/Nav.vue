ac39fa56d1c650586a2b178ccbb6df8550229c9a
Marinel maxime <maxime.m62@gmail.com>
2021-06-21 11:47:52 +0200

    prepare landing

---
+++ components/Nav.vue
@@ -0,0 +1,120 @@
+<template>
+  <nav id="header" class="w-full z-30 top-0 text-white py-1 lg:py-6">
+    <div
+      class="
+        w-full
+        container
+        mx-auto
+        flex flex-wrap
+        items-center
+        justify-between
+        mt-0
+        px-2
+        py-2
+        lg:py-6
+      "
+    >
+      <div class="pl-4 flex items-center">
+        <NuxtLink
+          class="
+            text-white
+            no-underline
+            hover:no-underline
+            font-bold
+            text-2xl
+            lg:text-4xl
+          "
+          to="/"
+        >
+          <img src="~/assets/cup.svg" height="32" width="32" />
+          Barista - Caffeine tracker
+        </NuxtLink>
+      </div>
+
+      <div class="block lg:hidden pr-4">
+        <!--
+          <button
+            id="nav-toggle"
+            class="
+              flex
+              items-center
+              px-3
+              py-2
+              border
+              rounded
+              text-gray-500
+              border-gray-600
+              hover:text-gray-800 hover:border-green-500
+              appearance-none
+              focus:outline-none
+            "
+          >
+            <svg
+              class="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>-->
+      </div>
+
+      <div
+        id="nav-content"
+        class="
+          w-full
+          flex-grow
+          lg:flex lg:items-center lg:w-auto
+          hidden
+          lg:block
+          mt-2
+          lg:mt-0
+          text-black
+          p-4
+          lg:p-0
+          z-20
+        "
+      >
+        <ul class="list-reset lg:flex justify-end flex-1 items-center">
+          <li class="mr-3">
+            <NuxtLink
+              class="inline-block py-2 px-4 text-black font-bold no-underline"
+              to="/privacy"
+              >Privacy</NuxtLink
+            >
+          </li>
+          <!--
+            <li class="mr-3">
+              <a
+                class="
+                  inline-block
+                  text-black
+                  no-underline
+                  hover:text-gray-800 hover:text-underline
+                  py-2
+                  px-4
+                "
+                href="#"
+                >link</a
+              >
+            </li>
+            <li class="mr-3">
+              <a
+                class="
+                  inline-block
+                  text-black
+                  no-underline
+                  hover:text-gray-800 hover:text-underline
+                  py-2
+                  px-4
+                "
+                href="#"
+                >link</a
+              >
+            </li>-->
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ pages/privacy.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="gradient leading-relaxed tracking-wide flex flex-col">
+    <Nav />
+    <div class="container mx-auto h-screen">
+      <div class="px-3 lg:px-0">
+        <div class="bg-gray-100 rounded-xl p-8">
+          <h1
+            class="
+              my-4
+              text-2xl
+              md:text-3xl
+              lg:text-5xl
+              font-black
+              leading-tight
+            "
+          >
+            Privacy
+          </h1>
+          Your privacy is paramount and that is why we do not collect any
+          personal information. We track the use of our application for
+          statistical purposes. You can deactivate this anonymous tracking at
+          any time in the app.
+        </div>
+
+        <div class="bg-gray-100 rounded-xl p-8 mt-8">
+          <h2 class="text-lg font-semibold">User Agreement</h2>
+          <h3 class="text-lg font-semibold">Subscription</h3>
+          Some features of the app you purchase a subscription. Payment will be
+          charged to your Apple ID account upon confirmation of purchase. Your
+          subscription renews automatically, unless it is canceled at least 24
+          hours before the end of the current period. Your account will be
+          charged for renewal within 24-hours prior to the end of the current
+          period. You can manage and cancel your subscriptions by going to your
+          account settings on the App Store after purchase. Subscription
+          payments are non-refundable.
+
+          <h3 class="text-lg font-semibold">Termination</h3>
+          We may, in our sole discretion, terminate or suspend your access to
+          all or part of the App at any time, with or without notice, for any
+          reason, including, without limitation, violation of these conditions.
+
+          <h3 class="text-lg font-semibold">Warning</h3>
+          YOU AGREE THAT YOUR USE OF THE APPLICATION WILL BE AT YOUR OWN RISK.
+          THE APPLICATION IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS FOR
+          YOUR USE. TO THE FULLEST EXTENT PERMITTED BY LAW, WE EXPRESSLY
+          DISCLAIM AND MAKE NO REPRESENTATIONS, WARRANTIES, CONDITIONS OR
+          COMMITMENTS OF ANY KIND, EXPRESS OR IMPLIED, ANNEXED TO THE
+          APPLICATION, BUT WITHOUT LIMITATION, LIMITATION OF ANY KIND. SALE
+          FITNESS FOR A PARTICULAR OR GENERAL PURPOSE, QUALITY, TITLE AND
+          NON-INFRINGEMENT. WE DO NOT WARRANT THAT ANY ERRORS OR DEFECTS IN THE
+          APPLICATION WILL BE CORRECTED AND DO NOT ASSUME RESPONSIBILITY FOR THE
+          CONDUCT OF USERS OF THE APPLICATION, ONLINE OR OFFLINE.
+
+          <h3 class="text-lg font-semibold">Limitation of Liability</h3>
+          YOU AGREE THAT, UNDER NO CIRCUMSTANCES, WE OR OUR RESPONSIBLE,
+          DIRECTORS, EMPLOYEES OR RESPECTIVE AGENTS WILL BE LIABLE TO YOU FOR
+          ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, PUNITIVE OR CONSEQUENTIAL
+          DAMAGES, WHATEVER ANY INDIRECT. BASED ON WARRANTY, CONTRACT, TORT OR
+          ANY OTHER LEGAL THEORY, AND WHETHER ARISING OUT OF LAW, STATUS, USE IN
+          TRADE, CUSTOMS, CONDUCT OR ENFORCEMENT, OR OF THE CONDUCT OR
+          COMMUNICATION OF THE PARTIES WITH ANOTHER, OR DUE TO THE NATURE OF
+          THESE TERMS OF USE OR IN CONFORMITY WITH USE, FAIRNESS OR THE LAW OR
+          OTHER, ARISING OUT OF OR IN CONNECTION WITH THE APPLICATION,
+          INCLUDING, BUT NOT LIMITED TO, ANY: (A) USE, INABILITY TO USE, OR
+          PERFORMANCE OF THE APPLICATION; (B) ERRORS, ERRORS OR INACCURACIES OF
+          THE CONTENT OR THE UNDERLYING SOFTWARE OF THE APP AND / OR (C)
+          INTERRUPTION OR TERMINATION OF SERVICE OR TRANSMISSION OF THE APP.
+
+          <h3 class="text-lg font-semibold">General provisions</h3>
+          These Terms, and any rights or licenses granted or waived herein, may
+          not be modified or assigned by you, but may be assigned by us without
+          restriction. In the event that any provision is found to be
+          unenforceable or invalid, that provision will still be applied to the
+          fullest extent permitted by applicable law, and such determination
+          will not affect the validity and enforceability of the remaining
+          remaining provisions of these Terms. Our failure to exercise or
+          enforce any right or provision of these Terms shall not create a
+          waiver of that right or provision. Headings used in these terms are
+          included for convenience only and do not limit or otherwise affect
+          these terms.
+        </div>
+      </div>
+    </div>
+    <!--<Footer />-->
+  </div>
+</template>
+
+<style scoped>
+html {
+  font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+}
+
+.gradient {
+  background-image: linear-gradient(-225deg, #cbbacc 0%, #2580b3 100%);
+}
+</style>
